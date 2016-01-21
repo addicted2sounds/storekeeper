6a55be8172b374a4d900d28ddc92068d3faf6a7d
addicted2sounds <addicted2sounds@gmail.com>
2016-01-21 18:50:50 +0200

    changed to show some options from products table

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  def product_option(product, option)
+    name = option.name.to_sym
+    return product.send(name) if product.respond_to? name
+    raw product.option(option).parsed_value
+  end
 end
